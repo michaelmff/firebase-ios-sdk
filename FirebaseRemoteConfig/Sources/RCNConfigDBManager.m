b146d0e6288d5dbbd5a69e2236c3ca291c7b3348
Michael McClung <michaelm@foreflight.com>
2022-07-18 15:46:40 -0500

    Fix sqlite db flags

--- FirebaseRemoteConfig/Sources/RCNConfigDBManager.m
+++ FirebaseRemoteConfig/Sources/RCNConfigDBManager.m
@@ -215,7 +215,6 @@ static NSArray *RemoteConfigMetadataTableColumnsInOrder() {
       return;
     }
     int flags = SQLITE_OPEN_CREATE | SQLITE_OPEN_READWRITE |
-                SQLITE_OPEN_FILEPROTECTION_COMPLETEUNTILFIRSTUSERAUTHENTICATION |
                 SQLITE_OPEN_FULLMUTEX;
     if (sqlite3_open_v2(databasePath, &strongSelf->_database, flags, NULL) == SQLITE_OK) {
       // Always try to create table if not exists for backward compatibility.
